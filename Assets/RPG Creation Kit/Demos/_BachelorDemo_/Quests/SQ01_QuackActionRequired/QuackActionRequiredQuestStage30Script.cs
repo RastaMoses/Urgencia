782b7c2b1838952e4dfcf678764a0e97aae062c9
Leon <leon@nyoucomer.de>
2026-05-26 13:18:50 +0200

    Addedflower and priest dialogue

---
+++ Assets/RPG Creation Kit/Demos/_BachelorDemo_/Quests/SQ01_QuackActionRequired/QuackActionRequiredQuestStage30Script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using RPGCreationKit;
+using RPGCreationKit.Quests;
+using RPGCreationKit.AI;
+using RPGCreationKit.CellsSystem;
+
+namespace RPGCreationKit.Quests
+{
+    public class QuackActionRequiredQuestStage30Script : QuestStageScript
+    {
+        [SerializeField] GameObject flowers;
+        private void Start()
+        {
+            // Your code here
+            //Spawn Flower Interactive Object
+            Instantiate(flowers);
+            //Disable Merchant
+
+
+            // Use this line to destroy the script if it is not longer needed.
+            Destroy(this);
+        }
+    }
+}
