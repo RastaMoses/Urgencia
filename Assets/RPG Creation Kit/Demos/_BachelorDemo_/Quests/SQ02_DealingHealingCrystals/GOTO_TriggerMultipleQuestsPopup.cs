c314848882c51305d174ab5bf20c901698155b3a
Leon <leon@nyoucomer.de>
2026-06-11 12:45:24 +0200

    Added mutliple quests popup

---
+++ Assets/RPG Creation Kit/Demos/_BachelorDemo_/Quests/SQ02_DealingHealingCrystals/GOTO_TriggerMultipleQuestsPopup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPGCreationKit;
+
+namespace RPGCreationKit.Game.ResultScripts
+{
+    public class GOTO_TriggerMultipleQuestsPopup : ResultScript
+    {
+        void Start()
+        {
+            // Your code here
+            TutorialAlertMessage.instance.OpenMessage("You have multiple quests active at the same time.\n\n\nTo track them you can open your journal with J.\n");
+
+
+            // Destroy the script
+            Destroy(this);
+        }
+    }
+}
